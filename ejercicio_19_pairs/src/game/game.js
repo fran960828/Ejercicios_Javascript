b392fb68652eb96856a265f7cb5d2ad84915a458
Francisco Navarro <franng96@hotmail.com>
2025-09-30 07:40:17 +0200

    juego de parejas finalizado

--- ejercicio_19_pairs/src/game/game.js
+++ ejercicio_19_pairs/src/game/game.js
@@ -35,48 +35,88 @@ class GameLogic {
   }
 
   createBoxes() {
-    const colors = this.createColors();
-    for (let i = 0; i < this.#rows; i++) {
-      for (let j = 0; j < this.#cols; j++) {
-        let color = colors.shift();
-        let box = new Box(i, j, color);
-        this.boxes.push(box);
+    if (localStorage.getItem("boxes") !== null) {
+      let boxes = JSON.parse(localStorage.getItem("boxes"));
+      boxes.map((box) => {
+        let newBox = new Box(box.row, box.col, box.color, box.open, box.match);
+        this.boxes.push(newBox);
+      });
+    } else {
+      const colors = this.createColors();
+      for (let i = 0; i < this.#rows; i++) {
+        for (let j = 0; j < this.#cols; j++) {
+          let color = colors.shift();
+          let box = new Box(i, j, color);
+          this.boxes.push(box);
+        }
       }
+      this.localStorageboxes();
     }
   }
 
   static getRowsCols() {
-    let rows = parseInt(prompt("Indica el número de filas"));
-    let cols = parseInt(prompt("Indica el número de columnas"));
-    while ((rows * cols) % 2 !== 0) {
-      rows = parseInt(prompt("Indica el número de filas"));
-      cols = parseInt(prompt("Indica el número de columnas"));
+    if (localStorage.getItem("rowscols")) {
+      return JSON.parse(localStorage.getItem("rowscols"));
+    } else {
+      let rows = parseInt(prompt("Indica el número de filas"));
+      let cols = parseInt(prompt("Indica el número de columnas"));
+      while ((rows * cols) % 2 !== 0) {
+        rows = parseInt(prompt("Indica el número de filas"));
+        cols = parseInt(prompt("Indica el número de columnas"));
+      }
+      localStorage.setItem("rowscols", JSON.stringify({ rows, cols }));
+
+      return { rows, cols };
     }
-    return { rows, cols };
   }
 
   // 🔑 Ahora la lógica de comparar cajas va aquí
   checkOpenBoxes() {
-    let openBoxes = this.boxes.filter(
-      (box) => box.open && box.match === false
-    );
+    let openBoxes = this.boxes.filter((box) => box.open && box.match === false);
 
     if (openBoxes.length === 2) {
-      const [box1,box2]=openBoxes
+      const [box1, box2] = openBoxes;
       if (box1.color === box2.color) {
         // ✅ Son iguales → marcamos como emparejadas
-        box1.match=true
-        box2.match=true
+        box1.match = true;
+        box2.match = true;
+        this.localStorageboxes();
       } else {
         // ❌ No son iguales → cerramos después de un tiempo
-       setTimeout(() => {
-        box1.resetcolor()
-        box2.resetcolor()
-        box1.open=false
-        box2.open=false
-       }, 500);
+        setTimeout(() => {
+          box1.resetcolor();
+          box2.resetcolor();
+        }, 500);
       }
+    } else {
+      this.localStorageboxes();
     }
+  }
+  checkFinishGame() {
+    let matchedboxes = this.boxes.filter((box) => box.match === false);
+    if (matchedboxes.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  localStorageboxes() {
+    let boxes = this.boxes.map((box) => {
+      return {
+        row: box.row,
+        col: box.col,
+        open: box.open,
+        match: box.match,
+        color: box.color,
+      };
+    });
+    localStorage.setItem("boxes", JSON.stringify(boxes));
+  }
+  removeLocalStorage() {
+    localStorage.removeItem("boxes");
+    localStorage.removeItem("rowscols");
+    localStorage.removeItem("timer");
+    location.reload();
   }
 }
 
@@ -98,11 +138,13 @@ class GameDom {
       let newBox = document.createElement("div");
       newBox.classList.add("box");
       box.element = newBox;
-
+      if (box.open || box.match) {
+        newBox.style.backgroundColor = box.color;
+      } else {
+        newBox.style.backgroundColor = "black";
+      }
       // Aquí es donde el box debería llamar a gameLogic.checkOpenBoxes()
-      box.eventChangeColor(() => {
-        this.gameLogic.checkOpenBoxes();
-      });
+      box.eventChangeColor();
 
       container.appendChild(newBox);
     });
@@ -110,4 +152,3 @@ class GameDom {
 }
 
 export { GameLogic, GameDom };
-
--- ejercicio_19_pairs/src/main.js
+++ ejercicio_19_pairs/src/main.js
@@ -1,5 +1,6 @@
 import "../sass/style.scss";
 import { GameLogic, GameDom } from "./game/game.js";
+import Timer from "./utils/timer.js";
 
 // Pedimos filas y columnas
 const { rows, cols } = GameLogic.getRowsCols();
@@ -12,9 +13,26 @@ const gameLogic = new GameLogic(rows, cols, tablero);
 
 // Creamos el DOM y pintamos
 const gameDom = new GameDom(gameLogic);
+// Creamos las cajas en la lógica
 gameLogic.createBoxes();
+// Pintamos las cajas en el DOM
 gameDom.paintboxes();
-tablero.addEventListener('click',()=>{
-gameLogic.checkOpenBoxes();
-})
+// Iniciamos el temporizador
+let temporizador = new Timer();
+temporizador.start();
+// Evento click en el tablero
+tablero.addEventListener("click", () => {
+  gameLogic.checkOpenBoxes();
+  if (gameLogic.checkFinishGame()) {
+    temporizador.stop();
+    gameLogic.removeLocalStorage();
+    alert("¡Has ganado!");
+  }
+});
+// Comprobar juego ganado
 
+// Botón de reset
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", () => {
+  gameLogic.removeLocalStorage();
+});
